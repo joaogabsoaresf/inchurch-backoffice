f068cd071805e0d29e6f7e60ce666df2859c8671
joaogabsoaresf <joao.soares@inchurch.com.br>
2025-04-28 15:49:01 -0300

    Refactor time zone setting to 'America/Sao_Paulo' and remove outdated card migration files

--- apps/base/models/time_stamped_model.py
+++ apps/base/models/time_stamped_model.py
@@ -1,14 +1,8 @@
 from django.db import models
-from django.utils import timezone
-
 from apps.base.models.active_model import ActiveModel
 
 
 class TimestampedModel(ActiveModel):
-    @staticmethod
-    def current_time_minus_3_hours():
-        return timezone.now() - timezone.timedelta(hours=3)
-
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
 
